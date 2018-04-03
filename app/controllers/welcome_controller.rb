e0289c64ebafecce131edefae1b89c2bee9dd45c
Vikas Naidu <vikas.naidu@cricbuzz.com>
2018-04-04 00:36:50 +0530

    performance improvement

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,11 @@ class WelcomeController < ApplicationController
 
   def index
     @users = User.all()
+    ids = []
+    @users.each do |user|
+      ids << user.id
+    end
+    @counts = Player.where(user_id:ids).group(:user_id).size
   end
 
 end
