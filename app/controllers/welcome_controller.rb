87e7b7f08f83cb1d14c34e53fd1980331bee01bd
Vikas Naidu <vikas.naidu@cricbuzz.com>
2018-04-05 18:23:57 +0530

    imporvments

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,25 @@
 class WelcomeController < ApplicationController
 
   def index
-    @users = User.all()
+    @users = User.where.not(id: 1)
+    @captains = {}
+    @vice_captains = {}
     ids = []
     @users.each do |user|
       ids << user.id
     end
-    @counts = Player.where(user_id:ids).group(:user_id).size
+    @players = Player.where.not(user_id: 1).group_by(&:user_id)
+      @players.each do |user_id, players|
+        players.each do |player|
+          if(player.is_captain)
+            @captains[user_id]= player
+          end
+          if(player.is_vice_captain)
+            @captains[user_id]= player
+          end
+        end
+      end
+
   end
 
 end
