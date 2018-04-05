92e79b9417efa657b4c1d8f02df3c8a192292cef
Vikas Naidu <vikas.naidu@cricbuzz.com>
2018-04-05 18:39:58 +0530

    refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,7 @@ class ApplicationController < ActionController::Base
     names = []
     response_hash["data"]["site"]["tour"]["match"]["players"].each {|x|
       team  = Team.find_or_create_by(name: x["squad"]["shortName"], id:x["squad"]["id"]   )
-      @player = Player.new()
-      puts @player.inspect
-      Player.create(name: x["name"], id: x["id"], team: team, user_id: 1, is_vice_captain:false, is_captain:false)
+      Player.find_or_create_by(name: x["name"], id: x["id"], team: team, user_id: 1, is_vice_captain:false, is_captain:false)
     }
     render plain: response_hash["data"]["site"]["tour"]["match"]["players"].to_json
   end
