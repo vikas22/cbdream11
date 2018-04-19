69e070e301a4ad1d4c63279d72c38f1ddc7ac235
Vikas Naidu <vikas.naidu@cricbuzz.com>
2018-04-19 16:58:52 +0530

    top11 score

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,25 +5,41 @@ class WelcomeController < ApplicationController
     @captains = {}
     @vice_captains = {}
     @total = {}
+    @top11 = {}
     @players = Player.where.not(user_id: 1).group_by(&:user_id)
       @players.each do |user_id, players|
         sum = 0
+        scores = []
         players.each do |player|
           total = player.player_scores.sum(:total)
+          score = 0
           if(player.is_captain)
-            sum = sum + (total * 2)
+            score = (total * 2)
+            sum = sum + score
             @captains[user_id]= player
           elsif(player.is_vice_captain)
-            sum = sum +  (total * 1.5)
+            score = (total * 1.5)
+            sum = sum + score
             @vice_captains[user_id]= player
           else
+            score = total
             sum = sum + total
           end
+          scores << score
         end
+        scores = scores.sort.reverse
+        index = if(scores.length >= 11)then 11 else scores.length end
+        top11Sum = 0
+        for i in 0..index
+            top11Sum = top11Sum + scores[i]
+        end
+        @top11[user_id] = top11Sum
         @total[user_id] = sum
       end
-      @total = @total.sort_by {|k, v| v}.reverse
-      puts @total
+
+      # @total = @total.sort_by {|k, v| v}.reverse
+      @top11 = @top11.sort_by {|k, v| v}.reverse
+
   end
 
 end
